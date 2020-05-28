0318df0cb4f36c55354a204f805c9fb1f30c3b78
llCnll <972581592@qq.com>
2020-05-28 15:52:05 +0800

    cloud-provider-payment8001注册到Euerka Server.

--- cloud-provider-payment8001/src/main/java/cn/chennan/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/cn/chennan/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package cn.chennan.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author cn
  * @date 2020-05-27 15:53
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
