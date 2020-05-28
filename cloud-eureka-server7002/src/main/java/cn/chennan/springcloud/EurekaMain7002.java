2aca355e158e6878f90da9483377924bdf7a93c1
llCnll <972581592@qq.com>
2020-05-28 17:05:25 +0800

    Eureka Server集群环境构建.

---
+++ cloud-eureka-server7002/src/main/java/cn/chennan/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package cn.chennan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author cn
+ * @date 2020-05-28 15:11
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
