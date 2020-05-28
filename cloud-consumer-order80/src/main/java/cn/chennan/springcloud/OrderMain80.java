0a5b5e041bd1763dee7dc2080173141a68bc0c0e
llCnll <972581592@qq.com>
2020-05-28 16:19:42 +0800

    cloud-consumer-order80注册到Euerka Server.

--- cloud-consumer-order80/src/main/java/cn/chennan/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/cn/chennan/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package cn.chennan.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author cn
  * @date 2020-05-28 10:32
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
