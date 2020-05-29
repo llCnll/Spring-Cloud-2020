7a7a6b3485bb1bc32cf64c04ae54a209f4521fa1
llCnll <972581592@qq.com>
2020-05-29 20:47:52 +0800

    cloud-providerconsul-payment8006注册到Consul.

---
+++ cloud-providerconsul-payment8006/src/main/java/cn/chennan/springcloud/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package cn.chennan.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author cn
+ * @date 2020-05-27 18:02
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/consul")
+    public String paymentConsul(){
+        return String.format("springcloud with consul: %s\t%s", serverPort, UUID.randomUUID().toString());
+    }
+}
