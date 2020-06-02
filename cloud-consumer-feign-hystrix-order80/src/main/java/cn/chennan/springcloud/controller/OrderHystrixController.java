336a545d85efe2345dca21b9b2d8c829ee9f7c4c
llCnll <972581592@qq.com>
2020-06-02 17:51:03 +0800

    80降级配置.

--- cloud-consumer-feign-hystrix-order80/src/main/java/cn/chennan/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/cn/chennan/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package cn.chennan.springcloud.controller;
 
 import cn.chennan.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,10 +26,17 @@ public class OrderHystrixController {
         return "cn.chennan.springcloud.controller.OrderHystrixController#paymentInfo_OK "+result;
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })
     @GetMapping(value = "/consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result =  paymentHystrixService.paymentInfo_TimeOut(id);
         log.info("****cn.chennan.springcloud.controller.OrderHystrixController#paymentInfo_TimeOut result = {}", result);
         return "cn.chennan.springcloud.controller.OrderHystrixController#paymentInfo_TimeOut "+result;
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return String.format("线程池: %s, paymentInfo_TimeOutHandler, 80繁忙, 等待重试.", Thread.currentThread().getName());
     }
 }
