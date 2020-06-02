907b0ae10df59e6988583e2fdbabb2e7f74ce59c
llCnll <972581592@qq.com>
2020-06-02 17:38:41 +0800

    8001降级配置.

--- cloud-provider-hystrix-payment8001/src/main/java/cn/chennan/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/cn/chennan/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package cn.chennan.springcloud.service.impl;
 
 import cn.chennan.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -16,13 +18,21 @@ public class PaymentServiceImpl implements PaymentService {
         return String.format("线程池: %s, paymentInfo_OK, id: %d", Thread.currentThread().getName(), id);
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     @Override
     public String paymentInfo_TimeOut(Integer id) {
+        System.out.println(String.format("线程池: %s, paymentInfo_TimeOut, 耗时: %d", Thread.currentThread().getName(), id));
         try {
             TimeUnit.SECONDS.sleep(id);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return String.format("线程池: %s, paymentInfo_TimeOut, 耗时: %d", Thread.currentThread().getName(), id);
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return String.format("线程池: %s, paymentInfo_TimeOutHandler, id: %d", Thread.currentThread().getName(), id);
     }
 }
