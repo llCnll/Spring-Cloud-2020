eddb25aacf30971f1291239f123854cf322fe5f5
llCnll <972581592@qq.com>
2020-06-04 19:04:53 +0800

    cloud-gateway-gateway9527 Filter.

---
+++ cloud-gateway-gateway9527/src/test/java/DateConvert.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author cn
+ * @date 2020-06-04 17:26
+ */
+public class DateConvert {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
