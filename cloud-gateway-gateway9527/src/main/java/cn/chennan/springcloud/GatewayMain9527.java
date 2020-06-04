6ce4d3842060c99489dcb42d03673837d3d826b2
llCnll <972581592@qq.com>
2020-06-04 11:54:23 +0800

    cloud-gateway-gateway9527.

---
+++ cloud-gateway-gateway9527/src/main/java/cn/chennan/springcloud/GatewayMain9527.java
@@ -0,0 +1,17 @@
+package cn.chennan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author cn
+ * @date 2020-05-28 10:32
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
