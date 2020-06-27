86dae562b29c4603a3b530b9dfebaa1dc6a78cfe
llCnll <972581592@qq.com>
2020-06-27 18:53:46 +0800

    cloud-config-center3344.

---
+++ cloud-config-center3344/src/main/java/cn/chennan/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package cn.chennan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author cn
+ * @date 2020-06-27 18:17
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
