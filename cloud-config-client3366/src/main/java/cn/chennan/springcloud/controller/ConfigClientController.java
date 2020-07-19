1b0d84dd4cf7438c62d3e2357343be5b7d59c026
llCnll <972581592@qq.com>
2020-07-19 11:07:20 +0800

    cloud-config-center3366. 整合Bus实现广播效果.

---
+++ cloud-config-client3366/src/main/java/cn/chennan/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package cn.chennan.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cn
+ * @date 2020-07-04 14:12
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return serverPort +": " + configInfo;
+    }
+}
