d8bab7a4b65b1f0b21a94ad5e54a5ef6d34967c7
llCnll <972581592@qq.com>
2020-07-26 17:18:06 +0800

    cloud-stream-rabbitmq-consumer8803.

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/cn/chennan/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package cn.chennan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cn
+ * @date 2020-07-26 16:45
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
